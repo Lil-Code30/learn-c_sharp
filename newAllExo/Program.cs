fe6624c3194966e2b825656592092619dbe15108
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-10-29 07:12:29 -0400

    SqueakyClean - Exercism Exo

---
+++ newAllExo/Program.cs
@@ -0,0 +1,18 @@
+﻿using newAllExo.ex;
+
+class Programm
+{
+    public static void Main()
+    {
+        string test1 = "my   ID";
+        string test2 = "my\0ID";
+        string test3 = "à-ḃç";
+        string test4 = "MyΟβιεγτFinder";
+
+        Console.WriteLine(SqueakyClean.Clean(test1));
+        Console.WriteLine(SqueakyClean.Clean(test2));
+        Console.WriteLine(SqueakyClean.Clean(test3));
+        Console.WriteLine( SqueakyClean.Clean(test4));
+       
+    }
+}
