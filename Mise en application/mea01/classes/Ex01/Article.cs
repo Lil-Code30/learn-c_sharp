47e3ee8eb2f2c2f21dc20d0434fa9b1b36a46a33
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-01-13 21:57:54 -0500

    feat : started mis en pratique 01 POOII

---
+++ Mise en application/mea01/classes/Ex01/Article.cs
@@ -0,0 +1,36 @@
+namespace Mise_en_application.mea01.classes.Ex01
+{
+    /* 
+        2. Créer une classe Article contenant :
+            a. Attributs : titre, quantité & prix unitaire
+            b. Méthodes : Afficher
+    */
+   public class Article
+    {
+        private string Titre;
+        private int Quantite;
+        private float PrixUnitaire;
+
+        public Article(string titre, int quantite, float prixUnitaire)
+        {
+            this.Titre = titre;
+            this.Quantite = quantite;
+            this.PrixUnitaire = prixUnitaire;
+        }
+
+        public float prixTotal()
+        {
+            return Quantite * PrixUnitaire;    
+        }
+
+        public void afficher()
+        {
+            Console.WriteLine($"Titre Article: {Titre}");
+            Console.WriteLine($"Quantité : {Quantite}");
+            Console.WriteLine($"Prix Unitaire : {PrixUnitaire}");
+            Console.WriteLine("================\n");
+        }
+
+        
+    } 
+}
