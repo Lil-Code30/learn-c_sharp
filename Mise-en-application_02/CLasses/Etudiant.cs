d13f521b958e8cc57fe1dc380869801453948623
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-01-14 16:16:04 -0500

    fix : mise en pratique 02 part 1 done 💯

--- Mise-en-application_02/CLasses/Etudiant.cs
+++ Mise-en-application_02/CLasses/Etudiant.cs
@@ -4,6 +4,34 @@ namespace Mise_en_application_02.CLasses
 {
     public class Etudiant
     {
-        private List<string> notes = new List<string>();
+        private List<float> notes;
+        private float moyenne;
+
+        public Etudiant()
+        {
+            this.notes = new List<float>();
+            this.moyenne = 0;
+        }
+
+        public void ajouteNote(float note)
+        {
+            notes.Add(note);
+        }
+
+        public float calculMoyenne()
+        {
+            int nombresDeNote = notes.Count();
+
+            float total = 0;
+
+            foreach(float note in notes)
+            {
+                total = total + note;
+            }
+
+             this.moyenne = total / nombresDeNote;
+
+            return moyenne;
+        }
     }
 }
