9c0ed773e15072d53e4e24344028610ab4c1dcd0
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-01-22 14:59:55 -0500

    feat : finished mise en applicatom sceance 02 ex 03

--- LABO/mise-en-application_02-ex03/Program.cs
+++ LABO/mise-en-application_02-ex03/Program.cs
@@ -2,5 +2,34 @@
 
 Duree duree01 = new Duree(20, 30, 30);
 Duree duree02 = new Duree(10, 10, 30);
+Duree duree03 = new Duree(00, 30, 30);
 
-Console.WriteLine(duree01 + duree02);
+Duree addition = duree01 + duree02;
+Duree soustraction = duree01 - duree02;
+
+Console.WriteLine("================ surcharges des opérateurs : Arithmétiques =================");
+
+Console.WriteLine("Addition des Duree : ");
+addition.Afficher();
+Console.WriteLine("+++++++++++++++++++++++++++++++++");
+Console.WriteLine("Soustraction des Duree : ");
+soustraction.Afficher();
+
+Console.WriteLine("=============================================================================");
+
+Console.WriteLine("================ surcharges des opérateurs : Comparaison =================");
+Console.WriteLine($"- Duree 01 equal to Duree 02 ? : {duree01 == duree02}");
+Console.WriteLine($"- Duree 01 not equal to Duree 02 ? : {duree01 != duree02}");
+Console.WriteLine($"- Duree 01 greater than Duree 02 ? : {duree01 > duree02}");
+Console.WriteLine($"- Duree 01 less than to Duree 02 ? : {duree01 < duree02}");
+Console.WriteLine($"- Duree 02 greater than Duree 01 ? : {duree02 > duree01}");
+Console.WriteLine($"- Duree 02 less than Duree 01 ? : {duree02 < duree01}");
+Console.WriteLine($"- Duree 01 greater than or equal to Duree 02 ? : {duree01 >= duree02}");
+Console.WriteLine($"- Duree 01 less than or equal to Duree 02 ? : {duree01 <= duree02}");
+Console.WriteLine($"- Duree 02 greater than or equal to Duree 01 ? : {duree02 >= duree01}");
+Console.WriteLine($"- Duree 02 less than or equal to Duree 01 ? : {duree02 <= duree01}");
+Console.WriteLine("=============================================================================");
+Console.WriteLine();
+Console.WriteLine("================ Convert Duree to Seconds =================");
+Console.WriteLine($"Convert Duree 01 to Seconds : {duree01.ToSecond()} Seconds");
+Console.WriteLine($"Convert Duree 03 to Seconds : {duree03.ToSecond()} Seconds");
--- LABO/mise-en-application_02-ex03/classes/Duree.cs
+++ LABO/mise-en-application_02-ex03/classes/Duree.cs
@@ -13,6 +13,22 @@ public class Duree
         Seconds = seconds;
     }
 
+    public void Afficher()
+    {
+        Console.WriteLine($"{Heures} heures, {Minutes} minutes and {Seconds} seconds");
+    }
+
+    public int ToSecond()
+    {
+        int hoursToSeconds = this.Heures * 3600;
+        int minutestoSeconds = this.Minutes * 60;
+
+        int dureeToSeconds = hoursToSeconds + minutestoSeconds + this.Seconds;
+        
+        return dureeToSeconds;
+    }
+    
+    // surcharges des opérateurs
     public static Duree operator +(Duree duree1, Duree duree2)
     {
         int hours = duree1.Heures + duree2.Heures;
@@ -20,5 +36,66 @@ public class Duree
         int seconds = duree1.Seconds + duree2.Seconds;
         
         return new Duree(hours, minutes, seconds);
+    }
+    
+    public static Duree operator -(Duree duree1, Duree duree2)
+    {
+        int hours = duree1.Heures - duree2.Heures;
+        int minutes = duree1.Minutes - duree2.Minutes;
+        int seconds = duree1.Seconds - duree2.Seconds;
+        
+        return new Duree(hours, minutes, seconds);
+    }
+
+    public static bool operator ==(Duree duree1, Duree duree2)
+    {
+        if (duree1.Heures == duree2.Heures && duree1.Minutes == duree2.Minutes && duree1.Seconds == duree2.Seconds)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public static bool operator !=(Duree duree1, Duree duree2)
+    {
+        return !(duree1 == duree2);
+    }
+
+    public static bool operator >(Duree duree1, Duree duree2)
+    {
+        if (duree1.Heures > duree2.Heures)
+        {
+            return true;
+        }
+            
+        return false;
+    }
+
+    public static bool operator <(Duree duree1, Duree duree2)
+    {
+        if (duree1.Heures < duree2.Heures)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool operator >=(Duree duree1, Duree duree2)
+    {
+        if (duree1.Heures >= duree2.Heures)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool operator <=(Duree duree1, Duree duree2)
+    {
+        if (duree1.Heures <= duree2.Heures)
+        {
+            return true;
+        }
+        return false;
     }
 }
