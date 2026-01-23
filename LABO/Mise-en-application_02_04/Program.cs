f9943efbc21a8983e9bd972a019d38ac547dc276
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-01-23 18:05:43 -0500

    feat : finished mise en application 02 ex 04

--- LABO/Mise-en-application_02_04/Program.cs
+++ LABO/Mise-en-application_02_04/Program.cs
@@ -1,1 +1,7 @@
-﻿namespace Mise_en_application_02_04.classes;
+﻿using Mise_en_application_02_04.classes;
+
+AbonnementAnnuel yearlyPlan = new AbonnementAnnuel();
+AbonnementMensuel monthlyPlan = new AbonnementMensuel();
+
+yearlyPlan.AfficherFacture();
+monthlyPlan.AfficherFacture();
