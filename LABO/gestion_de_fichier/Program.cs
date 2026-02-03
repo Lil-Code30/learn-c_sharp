41a9111cde342ee6e4d65d5fc8bb8bb248bfaa3c
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-02-03 15:22:36 -0500

    feat(test) : Écrire et lire dans fichier .txt

---
+++ LABO/gestion_de_fichier/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO; // include the System.IO namespace
+string directory = "/Users/ismaelloko/Downloads/_workstation/Workstation/C#/Learning/learn-c_sharp/LABO/gestion_de_fichier/repo";
+string filename = "myfile.txt";
+string filePath = directory + "/" + filename;
+
+// Directory.CreateDirectory(directory);
+//
+// Console.WriteLine($"Current Directory : {Directory.GetCurrentDirectory()}");
+
+// reading the file contain
+Console.WriteLine("=== Reading the file contain ===");
+if (File.Exists(filePath))
+{
+    string contents = File.ReadAllText(filePath);
+    Console.WriteLine(contents);
+}
+else
+{
+    Console.WriteLine("File not found");
+}
+
+string filename2 = "myFile_02.txt"; 
+string filePath2 = directory + "/" + filename2;
+string textToWrite = "Hello World!";
+
+Console.WriteLine("Writing in a file....");
+
+File.WriteAllText(filePath2, textToWrite);
+
+
+
