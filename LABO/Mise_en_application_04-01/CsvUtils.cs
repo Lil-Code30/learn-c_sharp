91a5c791e27b426d7f7498399fb671eae5eaa26c
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-02-04 05:13:23 -0500

    feat : finished mise en application sceance 04 Ex 01

--- LABO/Mise_en_application_04-01/CsvUtils.cs
+++ LABO/Mise_en_application_04-01/CsvUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿
+using System.Reflection;
 
 namespace Mise_en_application_04_01
 {
@@ -19,12 +20,66 @@ namespace Mise_en_application_04_01
 
         public static void CsvReader(string filePath)
         {
-            using( StreamReader reader = new StreamReader(filePath))
+            var tabHeader = new List<string>();
+
+            // list of rows for the table content
+            List<string[]> tabBody = new List<string[]>();
+
+            using ( StreamReader reader = new StreamReader(filePath))
             {
                 var lines = File.ReadAllLines(filePath).ToList();
+                int fileContentLenght = lines.Count;
+                
+                // getting the header from the file
+                string[] headers = lines[0].Split(",");
 
-                Console.WriteLine(lines[1]);
-                Console.WriteLine(lines.Count);
+                // adding the header in the Table header list
+                foreach (var h in headers)
+                {
+                    tabHeader.Add(h);
+                }
+
+                // fetching the rest of the file data and adding them to the table body
+                for (int i = 1; i < fileContentLenght; i++)
+                {
+                    var tabBodyLine = lines[i].Split(",");
+
+                    // here each file line is a row content for the table
+                    tabBody.Add(tabBodyLine);
+                }
+
+
+                Console.WriteLine("Displaying content from the csv file into a table");
+                Console.WriteLine();
+                Console.WriteLine("+----+----------+------------+--------+-------+------------+--------+-------+------------+");
+                Console.Write($"| ID |");
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    Console.Write($"| {headers[i]}    |");
+                }
+                Console.WriteLine();
+                Console.WriteLine("+----+----------+------------+--------+-------+------------+--------+-------+------------+");
+
+                int rowNum = 1;
+
+                // Displaying the csv content in a table format
+                foreach (var row in tabBody)
+                {
+                   
+                    Console.Write($"| {rowNum} ");
+
+                    for (int i = 0; i < row.Length; i++)
+                    {
+
+                        Console.Write($"| {row[i]}    |");
+
+                    }
+
+                    rowNum++;
+                    Console.WriteLine();
+                }
+                Console.WriteLine("+----+----------+------------+--------+-------+------------+--------+-------+------------+");
+
             }
         }
        
